4f4b1755ac95579b2a88a954c3a3db59bcfce656
URK21CO2007 <110015351+URK21CO2007@users.noreply.github.com>
2022-11-04 22:58:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,36 +1,44 @@
 from typing import List
-def merge(array, l, m, r):
-    array2 = [0 for i in range(r + 1)]
-    i = l
-    j = m + 1
-    k = l
-    while i <= m and j <= r:
-        if array[i] <= array[j]:
-            array2[k] = array[i]
-            i += 1
+
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
             else:
-            array2[k] = array[j]
-            j += 1
-        k += 1
-    if i > m:
-        while j <= r:
-            array2[k] = array[j]
-            j += 1
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
             k += 1
-    else:
-        while i <= m:
-            array2[k] = array[i]
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
             i += 1
             k += 1
-    for i in range(l, r + 1):
-        array[i] = array2[i]
-def merge_sort(array, l, r) -> None:
-  # Write code here
-  if l < r:
-        m = (l + r) // 2
-        merge_sort(array, l, m)
-        merge_sort(array, m + 1, r)
-        merge(array, l, m, r)
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
 
 
 # Do not change the following code
@@ -41,5 +49,5 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-merge_sort(data, 0, len(data) - 1)
+merge_sort(data)
 print(data)
